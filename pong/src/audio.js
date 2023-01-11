8cfb7294c2635614f9b7c22355efe8d79f96cb59
jenkernod <121118356+jenkernod@users.noreply.github.com>
2023-01-10 19:50:39 -0600

    Update audio.js

--- pong/src/audio.js
+++ pong/src/audio.js
@@ -39,10 +39,6 @@ export function playDeferredSounds() {
   }
 }
 
-export var soundArray = []; //list of sounds loaded
-
-//Here is where all the Sound File Players Start
-
 export var wallSound = new soundFile("tap-resonant.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
@@ -52,7 +48,17 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("phaserDown2.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("peaceful-ambience.mp3");
+export var ambientSound = new soundFile("peaceful-ambience");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -15; //turn down volume
+ambientSound.player.volume.value = -20; //turn down volume
+
+export var adventureMusic = new soundFile("peaceful-ambience.mp3");
+soundArray.push(adventureMusic);
+adventureMusic.player.loop = true;
+adventureMusic.player.volume.value = -0;
+
+export var villageMusic = new soundFile("peaceful-ambience.mp3");
+soundArray.push(villageMusic);
+villageMusic.player.loop = true;
+villageMusic.player.volume.value = -0;
